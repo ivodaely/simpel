5794ae62390380113f74839b88e2ba78b3d3dfdd
ivodaely <37564333+ivodaely@users.noreply.github.com>
2023-10-20 19:44:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
-<html>
-  <head>
-    <title>SIMPEL</title>
-  </head>
-  <body>
-    <h1><?php 
-    echo "What you say now ??";
-  ?></h1>
-  </body>
-</html>
+<?php
+  $host = "your_mysql_host";  // Hostname of your MySQL server (usually "localhost" on cPanel)
+  $username = "your_mysql_username";  // MySQL username
+  $password = "your_mysql_password";  // MySQL password
+  $database = "your_database_name";  // Name of your database
+
+  // Create a connection
+  $connection = mysqli_connect($host, $username, $password, $database);
+
+// Check the connection
+  if (!$connection) {
+      die("Connection failed: " . mysqli_connect_error());
+  }
+
+  // Connected successfully
+  echo "Connected to the MySQL database.";
+
+  // Now you can perform database operations here
+
+  // Close the connection when you're done
+  mysqli_close($connection);
+?>
+
+
